72767649dc15e6f30ac0ad10614c47e3a3a3bd2a
Mekdelawit Abadina <mekdelawitkassa6@gmail.com>
2025-07-18 00:49:02 +0300

    Time: 0 ms (100%), Space: 17.9 MB (35.03%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        
+        char_to_word = defaultdict(str)
+        word_to_char = defaultdict(str)
+        
+        for char, word in zip(pattern, words):
+            if char in char_to_word:
+                if char_to_word[char] != word:
+                    return False
+            else:
+                char_to_word[char] = word
+            
+            if word in word_to_char:
+                if word_to_char[word] != char:
+                    return False
+            else:
+                word_to_char[word] = char
+        
+        return True
