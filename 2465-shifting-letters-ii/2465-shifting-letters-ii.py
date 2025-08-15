01c6d355178bb937f31cd7cc2aa23f6d64c4f138
Mekdelawit Abadina <mekdelawitkassa6@gmail.com>
2025-08-16 00:33:30 +0300

    Time: 51 ms (88.29%), Space: 41.8 MB (24.68%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.py
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -1,17 +1,18 @@
-import numpy as np
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        prefix_sum = np.zeros(len(s), dtype=int)
+        prefix_sum=[0]*(len(s)+1)
         for start,end,direction in shifts:
             if direction==0:
-                prefix_sum[start:end+1]-=1   
+                prefix_sum[start]-=1
+                prefix_sum[end+1]+=1
             else:
-                prefix_sum[start:end+1]+=1
-        print(prefix_sum)    
+                prefix_sum[start]+=1
+                prefix_sum[end+1]-=1
         arr=[]
         shift=0
         for i in range(len(s)):
-            x=chr(((ord(s[i])- ord('a') + prefix_sum[i] )%26)+ord('a'))
+            shift += prefix_sum[i] 
+            x=chr(((ord(s[i])- ord('a') + shift )%26)+ord('a'))
             arr.append(x)
         return "".join(arr)
 
