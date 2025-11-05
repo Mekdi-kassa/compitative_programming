168073a273d4073c07073e1bfacfc5c8c6f61e9e
Mekdelawit Abadina <mekdelawitkassa6@gmail.com>
2025-11-05 17:22:30 +0300

    Time: 2358 ms (5%), Space: 482.3 MB (5.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,28 +1,23 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        memo  = {}
-        def coin(i ,ans):
+        memo = {}
+        def dp(i , ans):
             if ans == 0:
                 return 0
             
             if i >= n:
                 return float('inf')
-            
-            if ans < 0 :
-                return float('inf')
 
             if (i , ans) not in memo:
                 include = float('inf')
-                #to keep what we have 
                 if ans - coins[i] >= 0:
-                    include = 1 + coin(i, ans - coins[i])
-                # skip and go to the next without taking
-                exclude = coin(i + 1,ans)
-                memo[(i,ans)] = min(include,exclude)
-            return memo[(i,ans)]
-
-        res = coin(0,amount)
-        if res != float('inf'):
-            return res
-        return -1
+                    include = 1 + dp(i , ans - coins[i])
+                exclude = dp(i + 1 , ans)
+                memo[(i , ans)] = min(include , exclude)
+            return memo[(i ,ans)]
+        ans = dp(0 ,amount)
+        if ans != float('inf'):
+            return ans
+        return -1 
+            
