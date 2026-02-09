88ddb18f031ee0ecc4b828efbe1721f2cf7a4e13
Mekdelawit Abadina <mekdelawitkassa6@gmail.com>
2026-02-09 23:44:33 +0300

    Time: 11 ms (83.97%), Space: 22.3 MB (36.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,12 @@
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dict1 = defaultdict(list)
+        ans = []
         for word in strs:
-            x = "".join(sorted(word))
-            dict1[x].append(word)
-        arr = []
+            s = list(map(str,word.strip()))
+            s.sort()
+            w = "".join(s)
+            dict1[w].append(word)
         for val in dict1:
-            arr.append(dict1[val])
-        return arr
+            ans.append(dict1[val])
+        return ans
