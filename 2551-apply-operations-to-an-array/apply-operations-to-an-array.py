f3121b206f3bd17356a0ab35467f47b40045f0fd
Mekdelawit Abadina <mekdelawitkassa6@gmail.com>
2026-08-18 15:39:14 +0300

    Time: 3 ms (37.92%) | Memory: 19.1 MB (99.84%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,14 +1,13 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        for i in range(n -1):
-            if nums[i] == nums[i + 1]:
-                nums[i] = nums[i] * 2
-                nums[i + 1] = 0
-        # implemnting the movezero
-        first_zero = 0
-        for i in range(n):
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                nums[i] = 0
+                nums[i-1] = (nums[i-1] * 2)
+        pos = 0
+        for i in range(len(nums)):
             if nums[i] != 0:
-                nums[i] , nums[first_zero] = nums[first_zero] , nums[i]
-                first_zero += 1
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
+        
         return nums
